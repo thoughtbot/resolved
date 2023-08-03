e67912ada0ab23b5fc37d9e076517eea6c21a3c1
Steve Polito <stevepolito@hey.com>
2023-08-03 14:42:09 -0400

    Create simple homepage
    Return an HTML document by leveraging `ERB` to dynamically inject markup
    in the body. Note that we now return the correct HTTP Headers declaring
    the `Content-Type`.
    
    In order to ensure the body will be rendered correctly, we create a
    `render` method that is responsible for setting the `@content` used in
    `layout.html.erb` by setting it to the evaluated partial template.
    
    Additionally, we also use this new `render` method to set the headers
    and status code. This alleviates us from having to call `render` and
    then return the response.

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,26 @@
 require "bundler"
+require "erb"
 
 Bundler.require(:default, ENV.fetch("RACK_ENV").to_sym)
 
 class App
   def call(env)
-    [200, {}, ["Hello World"]]
+    render("home")
+  end
+
+  private
+
+  def render(template, status_code: 200)
+    @content = render_template(template)
+    body = render_template("layout")
+    headers = {"Content-Type" => "text/html; charset=utf-8"}
+
+    [status_code, headers, [body]]
+  end
+
+  def render_template(template)
+    template = File.read("./app/views/#{template}.html.erb")
+    erb = ERB.new(template)
+    erb.result(binding)
   end
 end
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -4,6 +4,7 @@ feature "App" do
   scenario "Visiting the homepage" do
     visit "/"
 
-    expect(page).to have_content "Hello World"
+    expect(page).to have_content "Resolved"
+    expect(page).to have_content "Enter a domain name"
   end
 end
